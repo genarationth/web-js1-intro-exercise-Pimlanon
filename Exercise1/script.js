f2c8f57dba914b8ccf8fe28633a9e114d5676a39
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-10 05:43:30 +0000

    add getElementById

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -1,8 +1,10 @@
 function ChangeName() {
     let text;
-    let person = prompt("What is your name:");
+    let person = prompt("What is your name", "Lak");
     console.log("The button has been clicked");
-
+    document.getElementById("demo")
+    .innerHTML = person;
+    
     // document.getElementById("demo").style.display = "block";
 }
 
